882ce92d51a2a42820b5ae82797a276d4e116241
nitronoid <jackdiver@hotmail.co.uk>
2017-12-06 00:09:56 +0000

    Replaced the table shader with a more generic tile shader. This was used for the back wall. The owl now lives in this simple scene.

--- scene.py
+++ scene.py
@@ -45,6 +45,7 @@ def camera_settings(ri, fov, maxsamples, pathlen, pixelvariance, pos, rot):
         vec = [x / rotation for x in rot]
         ri.Rotate(rotation, vec[0], vec[1], vec[2])
     ri.Exposure(1.0, 2.2)
+    ri.DepthOfField(0.15, 0.05, 14.5)
 
 
 def lighting(ri, env_strength, rx):
@@ -56,28 +57,31 @@ def lighting(ri, env_strength, rx):
     ri.AttributeEnd()
 
 
-def geometry(ri, rotate_x):
+def owl(ri, rotate_x):
     # The owl
     ri.AttributeBegin()
     ri.ShadingRate(0.5)
     ri.Rotate(rotate_x, 0, 1, 0)
     # Instantiate some patterns from compiled shaders
-    ri.Pattern('oiledWood', 'woodShader', {'float scale': [1.65],
-                                           'point translate': [-0.15, -0.1, 0],
-                                           'float warp': [1],
-                                           'float expo': [3],
-                                           'float thickness': [0.04],
-                                           'float gap': [0.2],
-                                           'float fuzz': [0.02]
+    ri.Pattern('oiledWood', 'woodShader', {'float eyeScale': [1.65],
+                                           'point eyeTranslate': [0.21, 0.3, 0],
+                                           'float eyeRotation': [7],
+                                           'float eyeWarp': [1],
+                                           'float eyeExponent': [3],
+                                           'float eyeThickness': [0.04],
+                                           'float eyeGap': [0.2],
+                                           'float eyeFuzz': [0.02]
                                            })
     # Apply displacement to the owl within 0.2 radius, lower this for faster renders
     ri.Attribute('displacementbound', {'float sphere': [0.2]})
     # Use the displace node with our pattern variables plugged in
-    ri.Displace('PxrDisplace', 'displace', {'float dispAmount': [-0.1], 'reference float dispScalar': ['woodShader:disp']})
+    ri.Displace('PxrDisplace', 'displace', {'float dispAmount': [-0.1],
+                                            'reference float dispScalar': ['woodShader:disp']
+                                            })
     # Use the PxrDisney for main qualities such as diffuse and spec
-    ri.Bxdf('PxrDisney', 'testShad', {'reference color baseColor': ['woodShader:resultRGB'], 
-                                      'float clearcoat' : [1],
-                                      'float clearcoatGloss' : [1],
+    ri.Bxdf('PxrDisney', 'testShad', {'reference color baseColor': ['woodShader:resultRGB'],
+                                      'float clearcoat': [0.8],
+                                      'float clearcoatGloss': [1],
                                       'reference float specular': ['woodShader:spec'],
                                       'reference float roughness': ['woodShader:rough']
                                       })
@@ -85,19 +89,40 @@ def geometry(ri, rotate_x):
     ri.ReadArchive('owl.rib')
     ri.AttributeEnd()
 
+
+def environment(ri):
     # Draw a floor plane
+    ri.TransformBegin()
+    ri.Rotate(7.5, 0, 1, 0)
+    ri.Scale(5, 1, 5)
     ri.AttributeBegin()
-    ri.Pattern('tableWood', 'table')
+    ri.Pattern('tile', 'table', {'float tile': [20], 'string name': ['Hackberry_pxr128']})
     ri.Bxdf('PxrDisney', 'testShad', {'reference color baseColor': ['table:resultRGB'],
-                                      'float specular': [0.8],
-                                      'float roughness': [0.6]
+                                      'float specular': [0.6],
+                                      'float roughness': [0.6],
+                                      'float clearcoat': [1],
+                                      'float clearcoatGloss': [1]
                                       })
     ri.Attribute('displacementbound', {'float sphere': [0.2]})
     # Use the displace node with our pattern variables plugged in
-    ri.Displace('PxrDisplace', 'displace', {'float dispAmount': [0.1], 'reference vector dispVector': ['table:resultDisp']})
-    ri.Scale(5, 1, 5)
+    ri.Displace('PxrDisplace', 'displace', {'float dispAmount': [0.1],
+                                            'reference vector dispVector': ['table:resultDisp']
+                                            })
     ri.Patch('bilinear', {'P': [10, -3.15, 10, 10, -3.15, -10, -10, -3.15, 10, -10, -3.15, -10]})
     ri.AttributeEnd()
+    ri.AttributeBegin()
+    ri.Pattern('tile', 'wall', {'float tile': [15], 'string name': ['White_Stucco']})
+    ri.Bxdf('PxrDisney', 'testShad', {'reference color baseColor': ['wall:resultRGB'],
+                                      'float specular': [0.6],
+                                      'float roughness': [0.6],
+                                      'float clearcoat': [1],
+                                      'float clearcoatGloss': [1]
+                                      })
+    ri.Translate(0, 0, 3.65)
+    ri.Rotate(90, 1, 0, 0)
+    ri.Patch('bilinear', {'P': [10, -3.15, 10, 10, -3.15, -10, -10, -3.15, 10, -10, -3.15, -10]})
+    ri.AttributeEnd()
+    ri.TransformEnd()
 
 
 def scene(name, rx, ex, save):
@@ -111,7 +136,7 @@ def scene(name, rx, ex, save):
     ri.Begin('__render')
 
     # Set up image writing
-    output_options(ri, filename=name, res=(640, 480, 1), save=save)
+    output_options(ri, filename=name, res=(853, 480, 1), save=save)
 
     # Camera settings
     camera_settings(ri,
@@ -119,8 +144,8 @@ def scene(name, rx, ex, save):
                     maxsamples=2048,
                     pathlen=2,
                     pixelvariance=0.01,
-                    pos=(0, 0, 17),
-                    rot=(-10, 5, 0)
+                    pos=(0, 0, 16),
+                    rot=(-7.5, 0, 0)
                     )
 
     # Start of the scene
@@ -128,8 +153,10 @@ def scene(name, rx, ex, save):
 
     # Scene lighting
     lighting(ri, env_strength=5, rx=ex)
-    # All scene geometry
-    geometry(ri, rotate_x=rx)
+    # Draw the owl
+    owl(ri, rotate_x=rx)
+    # Draw the environment geometry
+    environment(ri)
 
     # End of scene and RIB file
     ri.WorldEnd()
